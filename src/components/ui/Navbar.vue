00115d6ebadbe88e56c3afba6081b602581b4a35
Hector Perez M <hector.perez.m@usach.cl>
2019-07-23 17:18:02 -0400

    Navbar header avance, arreglar iconos

--- src/components/ui/Navbar.vue
+++ src/components/ui/Navbar.vue
@@ -1,19 +1,23 @@
 <template>
-    <v-parallax :src="images.background" height="500">
+    <v-parallax :src="images.background" height="100%" class="nav-parallax">
         <v-layout row wrap>
             <v-flex xs8>
-                <h1>GPUVMEM LOGO</h1>
+                <v-img :src="images.logo" height="300" width="350" position="top 0px left 0px" class="nav-logo"></v-img>
             </v-flex>
             <v-flex xs4>
-                <v-layout row wrap>
+                <v-layout row wrap class="nav-links">
                     <v-flex>
-                        <h2>Link 1</h2>
+                        <h3>DOCUMENTATION</h3>
                     </v-flex>
                     <v-flex>
-                        <h2>Link 2</h2>
+                        <h3>SUPPORT US</h3>
                     </v-flex>
                     <v-flex>
-                        <h2>Link 3</h2>
+                        <h3>CONTACT US</h3>
+                    </v-flex>
+                    <v-flex>
+<!--                        TODO arreglar logo, no se ve-->
+                        <h3>GITHUB</h3>
                     </v-flex>
                 </v-layout>
             </v-flex>
@@ -29,20 +33,26 @@
         name: 'navbar',
         data: () => ({
             images: {
-                logo: require('@/assets/logo_gpuvmem.png'),
-                background: require('@/assets/astronomy_image_1.jpg')
+                logo: require('@/assets/logo_gpuvmem_white.png'),
+                background: require('@/assets/astronomy_image_2.jpg')
             }
         })
     }
 </script>
 
 <style scoped>
-    .nav-container {
-        background-color: #a8abaa;
+    .nav-parallax {
+        height: 600px;
+        opacity: 0.95;
     }
 
-    .nav-header {
-        color: white;
-        background-color: #a8abaa;
+    .nav-logo {
+
     }
+
+    .nav-links {
+        font-size: 14px;
+        margin-top: 150px;
+    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* Vuetify*/
 import Vuetify from 'vuetify'
 import './plugins/vuetify'
-
-Vue.use(Vuetify)
 
 /* Router */
 import router from './router'
 
+/* Font Awesome*/
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
