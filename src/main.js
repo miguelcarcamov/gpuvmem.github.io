1303348e5583cc19e7aa7a29d22ed77398c160c5
Javier <javier.arredondo.c@usach.cl>
2019-07-30 16:55:30 -0400

    CHG: Header changed for some responsive

--- src/main.js
+++ src/main.js
@@ -4,13 +4,17 @@ import App from './App.vue'
 /* Vuetify */
 import vuetify from './plugins/vuetify';
 
-/* Rotuer */
+/* Router */
 import router from './router'
+
+/* Vuex */
+import store from './store/store'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
